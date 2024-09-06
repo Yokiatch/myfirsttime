733d410aec058a60e0f61150a5a7d2e2b133681e
Dinesh <dkkhatana136@gmail.com>
2024-09-06 12:56:23 +0530

    Initialcommit

---
+++ .vscode/lifesucks.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+         int n;
+         cin>>n;
+         int arr[n];
+         for(int i=0; i<n; i++){
+               cin>>arr[i];
+         }
+         int count = 0;
+         for(int i=0; i<n; i++){
+               for(int j=i+1; j<n; j++){
+                    if(arr[i]*arr[j]==(i+1)+(j+1)){
+                         count++;
+                    }
+                    if (arr[i] * arr[j] > i + j + 2) {
+                    break;}
+               }
+         }
+         cout<<count<<endl;
+     }
+     
+     return 0;
+}
